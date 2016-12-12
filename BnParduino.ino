56678613b13a958d7e8a27f36c1ae9a14e0bac59
Luka Lazovic <lukaorigin97@hotmail.com>
2016-12-12 22:55:06 +0100

    Add files via upload

---
+++ BnParduino.ino
@@ -0,0 +1,19 @@
+#include <Servo.h>
+
+Servo servo1;
+Servo servo2;
+
+int pos = 0;
+
+void setup() {
+  servo1.attach(11);
+  servo2.attach(10);
+  Serial.begin(9600);
+}
+
+void loop() {
+  while (Serial.available() <= 0);
+  servo1.write(Serial.read());
+  while (Serial.available() <= 0);
+  servo2.write(Serial.read());
+}
